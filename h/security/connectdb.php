91c71ffe89863824a7f95f99b3c9d86bde368ff3
jeerawat-2547 <66010914003@msu.ac.th>
2026-02-04 17:10:27 +0700

    aasd

--- h/security/connectdb.php
+++ h/security/connectdb.php
@@ -1,7 +1,7 @@
 <?php
 		$host = "localhost";
 		$user = "root";
-		$pwd = "";
+		$pwd = "66010914003";
 		$db = "4003db";
 		$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
 		mysqli_query($conn, "SET NAMES utf8");
