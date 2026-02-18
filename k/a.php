e254837b047ecf5f261f0576399759b0dfb631f6
jeerawat-2547 <66010914003@msu.ac.th>
2026-02-18 16:48:41 +0700

    asd

---
+++ k/a.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>จีรวัฒน์ ศักดิ์วงษ์ (อิคคิว)</title>
+</head>
+
+<body>
+    <h1>จีรวัฒน์ ศักดิ์วงษ์ (อิคคิว)</h1>
+<button type = "submit" name="Submit" img src="images/1.jpg" width="400"> รูปจีรวัฒน์ ศักดิ์วงษ์ (อิคคิว)</button><br>
+<button type = "submit" name="Submit" img src="images/j.jpg" width="400"> รูปอาจารย์เอกชัย </button>
+
+
+</body>
+</html>
